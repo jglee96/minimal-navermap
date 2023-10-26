742ff1e9d385939e999d0ccb74b204ae332568d1
jglee96 <zakklee96@gmail.com>
2023-10-26 11:36:22 +0900

    add: 고정된 사각형 예제로 수정
    * 픽셀 크기가 고정된 사각형 예제 테스트
    * zoom, map 이동으로 인한 map의 bounds_changed event에서 rect의
    bounds를 수정하는 것으로 픽셀 크기 고정하려고 함
    * 현재 zoom, map 이동으로 인해 모두 bounds_changed가 trigger 되지만
    zoom에서만 setBounds가 적용되고 map 이동에서는 setBounds가 적용되지
    않고 있음

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import "./main.css";
+import { debounce } from "throttle-debounce";
+
 const NAVER_MAP_API = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${
   import.meta.env.VITE_OAPI_KEY
 }&submodules=geocoder,drawing`;
@@ -11,24 +14,49 @@ if (script === null) {
 }
 
 script.onload = () => {
-  var map = new naver.maps.Map("map");
-
-  const mapCenter = map.getCenter();
-
-  const input = document.createElement("input");
-  input.setAttribute("type", "number");
-
-  const marker = new naver.maps.Marker({
-    map: map,
-    position: mapCenter,
-    icon: {
-      content: /*html*/ `
-      <input id="input" type="number" />
-      `,
-    },
+  const map = new naver.maps.Map("map", {
+    zoom: 19,
   });
+  const projection = map.getProjection();
 
-  document
-    .getElementById("input")
-    .addEventListener("mousedown", (e) => e.stopPropagation());
+  naver.maps.Event.once(map, "init", () => {
+    const sideLength = Math.sqrt(5000);
+
+    const mapCenter = map.getCenter();
+    const c = naver.maps.TransCoord.fromLatLngToUTMK(mapCenter);
+    const lt = naver.maps.TransCoord.fromUTMKToLatLng(
+      c.clone().add(sideLength / 2, sideLength / 2)
+    );
+    const rb = naver.maps.TransCoord.fromUTMKToLatLng(
+      c.clone().sub(sideLength / 2, sideLength / 2)
+    );
+
+    const off1 = projection.fromCoordToOffset(lt);
+    const off2 = projection.fromCoordToOffset(rb);
+
+    console.log(off1, off2);
+
+    const bounds = new naver.maps.LatLngBounds(lt, rb);
+
+    const rect = new naver.maps.Rectangle({
+      map,
+      bounds,
+      fillColor: "#6741D9",
+      fillOpacity: 0.3,
+      strokeWeight: 2,
+      strokeColor: "#6741D9",
+    });
+
+    const func = () => {
+      const bounds = new naver.maps.LatLngBounds(
+        projection.fromOffsetToCoord(off1),
+        projection.fromOffsetToCoord(off2)
+      );
+      rect.setBounds(bounds);
+    };
+
+    const throttleFun = debounce(100, func);
+
+    naver.maps.Event.addListener(map, "bounds_changed", func);
+  });
 };
